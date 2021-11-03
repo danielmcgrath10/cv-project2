8bf11a2b24bd6d05ac424682f19e7e02d904a0e3
Daniel McGrath <danmcgrath1035@gmail.com>
2021-11-03 16:10:30 -0400

    adding gitgnore

---
+++ part1.m
@@ -0,0 +1,28 @@
+
+directory = uigetdir(pwd, 'Select a Folder');
+filePattern = fullfile(directory, '*.jpg');
+imageFiles = dir(filePattern);
+numImages = length(imageFiles);
+    
+start = 1;
+stop = 2; 
+k_val = 0.04;
+    
+for i = start:stop
+  file = imageFiles(i);
+  images(:,:,i) = rgb2gray(imread(strcat(file.folder,'/',file.name)));
+end
+
+corners_1 = detectHarrisFeatures(images(:,:,1));
+corners_2 = detectHarrisFeatures(images(:,:,2));
+
+% imshow(images(:,:,1)); hold on;
+% plot(corners_1.selectStrongest(50));
+% hold off;
+
+figure;
+imshow(images(:,:,2)); hold on;
+plot(corners_2.selectStrongest(50));
+hold off; 
+
+
