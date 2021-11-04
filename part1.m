ecaaac6a5afbfef2b33792dd33ee362461b1e260
Daniel McGrath <danmcgrath1035@gmail.com>
2021-11-04 12:43:44 -0400

    adding logic

--- part1.m
+++ part1.m
@@ -6,6 +6,8 @@
 ratio = 0.1;
 corners_1 = detectHarrisFeatures(images(:,:,1), 'MinQuality', ratio);
 corners_2 = detectHarrisFeatures(images(:,:,2), 'MinQuality', ratio);
+ncc_thresh = 10;
+ncc_mesh = 1;
 
 ratio = 0.4;
 
@@ -20,12 +22,15 @@ corners_2_nonmax = nonmax_suppression(corners_2, nonmax_distance);
 corners1 = round(corners_1.Location);
 corners2 = round(corners_2.Location);
 
-nnc = zeros(length(corners1));
+ncc = zeros(length(corners1), length(corners2));
+im1 = images(:,:,1);
+im2 = images(:,:,2);
 for i = 1:length(corners1)
-    a = corners1(i);
+    a = corners1(i,:);
     for j = 1:length(corners2)
-        b = corners2(j);
-        
+        b = corners2(j,:);
+        n1 = im1(a(1)-ncc_mesh:a(1)+ncc_mesh,a(2)-ncc_mesh:a(2)+ncc_mesh);
+        n2 = im2(b(1)-ncc_mesh:b(1)+ncc_mesh,b(2)-ncc_mesh:b(2)+ncc_mesh);
     end
 end
 
