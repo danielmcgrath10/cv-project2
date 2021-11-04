f920bbc8d7a59e327110dbc0cf12a950e1babf83
Daniel McGrath <danmcgrath1035@gmail.com>
2021-11-04 13:05:27 -0400

    changes

--- part1.m
+++ part1.m
@@ -6,7 +6,7 @@
 ratio = 0.1;
 corners_1 = detectHarrisFeatures(images(:,:,1), 'MinQuality', ratio);
 corners_2 = detectHarrisFeatures(images(:,:,2), 'MinQuality', ratio);
-ncc_thresh = 10;
+ncc_thresh = 0.5;
 ncc_mesh = 1;
 
 ratio = 0.4;
@@ -29,8 +29,18 @@ for i = 1:length(corners1)
     a = corners1(i,:);
     for j = 1:length(corners2)
         b = corners2(j,:);
-        n1 = im1(a(1)-ncc_mesh:a(1)+ncc_mesh,a(2)-ncc_mesh:a(2)+ncc_mesh);
-        n2 = im2(b(1)-ncc_mesh:b(1)+ncc_mesh,b(2)-ncc_mesh:b(2)+ncc_mesh);
+%         if((a(1)-ncc_mesh < 1) || (a(2)-ncc_mesh < 1) || (a(1)+ncc_mesh > length(im1)) || (a(2)+ncc_mesh > length(im1)))
+%             break
+%         end
+        n1 = im1(a(2)-ncc_mesh:a(2)+ncc_mesh,a(1)-ncc_mesh:a(1)+ncc_mesh);
+%         if((b(1)-ncc_mesh < 1) || (b(2)-ncc_mesh < 1) || (b(1)+ncc_mesh > length(im2)) || (b(2)+ncc_mesh > length(im2)))
+%             break
+%         end
+        n2 = im2(b(2)-ncc_mesh:b(2)+ncc_mesh,b(1)-ncc_mesh:b(1)+ncc_mesh);
+        norm = max(max(normxcorr2(n1,n2)));
+        if(norm > ncc_thresh)
+            ncc(i,j) = norm;
+        end
     end
 end
 
