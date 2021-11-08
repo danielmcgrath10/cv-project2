6740fc1d4943be037c084c8029a5e59236ed5879
Daniel McGrath <danmcgrath1035@gmail.com>
2021-11-07 19:57:18 -0500

    changes

--- part1.m
+++ part1.m
@@ -38,4 +38,16 @@ imshow(rgb_images(:,:,:,2)); hold on;
 plot(corners_2_nonmax);
 hold off;
 
+h = inv(ransac_H);
+im1 = images(:,:,1);
+im = double(images(:,:,2));
+[xi,yi] = meshgrid(1:size(im, 1), 1:size(im, 2));
+xx = (h(1,1) * xi + h(1,2) * yi + h(1,3))./(h(3,1) * xi + h(3,2) * yi + h(3,3));
+yy = (h(2,1) * xi + h(2,2) * yi + h(2,3))./(h(3,1) * xi + h(3,2) * yi + h(3,3));
+foo = uint8(interp2(im, xx, yy));
+figure;
+montage({im1, foo});
+
+overlap = 
+
 
